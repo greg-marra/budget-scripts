cdb1d2e370ee00e4c81e13f68adb5116d0033a35
Greg Marra <gmarra36@gmail.com>
2021-04-26 09:55:30 -0400

    report to get total moved into etrade account

---
+++ etrade.php
@@ -0,0 +1,44 @@
+<?php
+
+    $base_dir = $_SERVER['HOME'];
+    require ($base_dir . '/Documents/ynab_vars.php');
+    require ($base_dir . '/Documents/ynab/ynab_functions.php');
+    $report_name = "ETrade Amount Invested";
+
+    # Get latest date for budget in YNAB and set that in GET for category balances
+    $settings = get_settings($ch, $base);
+    $oldest_parsed_date = get_oldest_date($settings);
+    $recent_parsed_date = get_recent_date($settings);
+
+    # Endpoint to grab all transactions for 'Credit Card Cash Rewards'
+    $endpoint = "/$BUDGET_ID/accounts/$ETRADE_ACCOUNT_ID/transactions";
+    curl_setopt($ch, CURLOPT_URL, $base . $endpoint);
+
+    $transactions = json_decode(curl_exec($ch), true);
+
+    $yearly_totals = array();
+
+    foreach ($transactions["data"]["transactions"] as $transaction) {
+
+        if ($transaction["payee_id"] == $ETRADE_TRANSFER_PAYEE_ID) {
+        
+            #var_dump($transaction);echo "\n\n\n\n\n";
+
+            $amount = $transaction["amount"] / 1000;
+            $year = explode("-", $transaction["date"])[0];
+
+            if (array_key_exists($year, $yearly_totals)) {
+                
+                $yearly_totals[$year] += $amount;
+
+            } else {
+
+                $yearly_totals[$year] = $amount;
+
+            }
+
+        }
+
+    }
+
+    print_totals($yearly_totals, $report_name, $oldest_parsed_date, $recent_parsed_date);
