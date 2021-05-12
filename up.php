c443a50a6a3ae9e0612df42e45106475f510a22f
Greg Marra <gmarra36@gmail.com>
2021-05-11 21:27:01 -0400

    up.php for troubleshooting split transactions

---
+++ up.php
@@ -0,0 +1,51 @@
+<?php
+
+    $base_dir = $_SERVER['HOME'];
+    require ($base_dir . '/Documents/ynab_vars.php');
+    require ($base_dir . '/Documents/ynab/ynab_functions.php');
+
+    # Imports Name and Colors of each flag
+    require ($base_dir . '/Documents/ynab_flags.php');
+    $report_name = "Flagged Report";
+
+    # Get latest date for budget in YNAB and set that in GET for category balances
+    $settings = get_settings($ch, $base);
+    $oldest_parsed_date = get_oldest_date($settings);
+    $recent_parsed_date = get_recent_date($settings);
+
+    # Endpoint to grab all transactions for 'Credit Card Cash Rewards'
+    $endpoint = "/$BUDGET_ID/transactions";
+    curl_setopt($ch, CURLOPT_URL, $base . $endpoint);
+
+    $color = "orange";
+    $transaction_ids = array();
+    $transactions_to_update = array();
+
+    $transactions = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+
+    foreach ($transactions["data"]["transactions"] as $transaction) {
+
+        if ($transaction["flag_color"] === $color) {
+
+                array_push($transaction_ids, substr($transaction["id"],0,36));
+                $wrapped = array("transaction" => $transaction);
+                array_push($transactions_to_update, $wrapped);
+
+            }
+
+    }
+
+    foreach ($transactions_to_update as &$value) {
+
+        $value["transaction"]["date"] = date("Y-m-d");
+        $data_json = json_encode($value);
+
+        var_dump($data_json);
+        echo $base . $endpoint . "/" . $value["transaction"]["id"];
+        $ch_put = set_curl_put($data_json, $YNAB_TOKEN, $base . $endpoint . "/" . $value["transaction"]["id"]);
+        $response = put_curl($ch_put);
+        var_dump($response);echo "\n\n\n\n\n";
+        curl_close($ch_put);
+
+    }
