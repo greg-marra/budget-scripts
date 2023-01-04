f319e227aad541d2520dc657c13df97623c1cd12
Greg Marra <gmarra36@gmail.com>
2023-01-03 20:09:38 -0500

    adds budget IDs to playground report

--- playground.php
+++ playground.php
@@ -4,8 +4,17 @@
     require ($base_dir . '/Documents/vars-budget.php');
     require ($base_dir . $functions_directory);
 
-    # Endpoint to grab list of category
+    # Endpoint to grab all transactions
     $endpoint = "/$BUDGET_ID/transactions";
+
+    curl_setopt($ch, CURLOPT_URL, $base . $endpoint);
+    $array = json_decode(curl_exec($ch), true);
+    var_dump($array);
+
+    echo "\n" . $base . $endpoint . "\n";
+
+    # Endpoint to grab list of category
+    $endpoint = "/$BUDGET_ID/categories";
 
     curl_setopt($ch, CURLOPT_URL, $base . $endpoint);
     $array = json_decode(curl_exec($ch), true);
